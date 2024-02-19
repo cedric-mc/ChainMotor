d13d258d2dea93f262650650cccb7878b67aa4a1
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-02-19 09:16:11 +0100

    19/02/24 09:16
    java args update

--- src/main/java/fr/uge/main/Main.java
+++ src/main/java/fr/uge/main/Main.java
@@ -20,9 +20,8 @@ public class Main {
     /**
      * Méthode principale :
      * @param args
-     * args[0] : Nom du fichier C : game_data_[pseudo].txt
-     * args[1] : Nom du fichier Java : mst_[pseudo].txt
-     * args[2] : Nom du fichier de sortie : best_path_[pseudo].txt
+     * args[0] : pseudo du joueur
+     * args[1] : etat du jeu (1er tour ou ajout de mot), 0 pour le 1er tour, 1 pour l'ajout de mot
      * @throws IOException
      *
      * Récupèration ou création d’un arbre recouvrant maximal et recherche du meilleur chemin entre le mot de départ et de cible.
@@ -33,17 +32,16 @@ public class Main {
             System.out.println("Auteurs :\nMamadou BA\nCédric MARIYA CONSTANTINE\nAbdelrahim RICHE\nVincent SOUSA\nYacine ZEMOUCHE");
             return;
         }
-        // Si le nombre d’arguments est différent de 3
-        if (args.length != 3) {
-            System.out.println("Utilisation : java -cp ChainMotor/target/classes fr.uge.main.Main partie/game_data_[pseudo].txt partie/mst_[pseudo].txt partie/best_path_[pseudo].txt");
+        // Si le nombre d'arguments est différent de 2
+        if (args.length != 2) {
+            System.out.println("Utilisation : java -cp ChainMotor/target/classes fr.uge.main.Main [pseudo] etat");
             return;
         }
-
-        String fileNameC = args[0]; // Nom du fichier C : game_data_[pseudo].txt
-        String fileNameJava = args[1]; // Nom du fichier Java : mst_[pseudo].txt
-        String fileNameOutput = args[2]; // Nom du fichier de sortie : best_path_[pseudo].txt
+        String fileNameC = FileLine.GAME_FILE_C.line + args[0] + FileLine.GAME_FILE_EXTENSION.line; // Nom du fichier C : game_data_[pseudo].txt
+        String fileNameJava = FileLine.GAME_FILE_JAVA.line + args[0] + FileLine.GAME_FILE_EXTENSION.line; // Nom du fichier Java : mst_[pseudo].txt
+        String fileNameOutput = FileLine.GAME_FILE_OUTPUT.line + args[0] + FileLine.GAME_FILE_EXTENSION.line; // Nom du fichier de sortie : best_path_[pseudo].txt
         MaximumSpanningTree maximumSpanningTree;
-        if (!Files.exists(Path.of(fileNameJava))) { // On vérifie que les fichiers existent
+        if (args[1] == "0") {
             // Création de l’arbre recouvrant maximal et exportation dans un fichier
             maximumSpanningTree = MaximumSpanningTree.createMaximumSpanningTree(fileNameC);
         } else { // Sinon, on charge l’arbre recouvrant maximal
