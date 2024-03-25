c2aeda031c02a5189753cdb150baaaf53dbfb450
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-25 15:26:10 +0100

    Ajouter les dépendances JUnit 4.13.1 et tester la création d'un mot

--- src/test/java/fr/uge/semonkey/test/WordTest.java
+++ src/test/java/fr/uge/semonkey/test/WordTest.java
@@ -1,2 +1,14 @@
-package fr.uge.semonkey.test;public class WordTest {
+package fr.uge.semonkey.test;
+
+import fr.uge.semonkey.model.Word;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class WordTest {
+    @Test
+    public void testWordCreation() {
+        Word word = new Word("chien");
+        assertNotNull(word);
+        assertEquals("chien", word.word());
+    }
 }
